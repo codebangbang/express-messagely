76f2d6ba7f2efe9953e06044e6309171f9f7fac2
ryanfair <ryfair@gmail.com>
2024-04-04 15:58:45 -0400

    Step 2 Completed

--- models/user.js
+++ models/user.js
@@ -10,20 +10,46 @@ class User {
    *    {username, password, first_name, last_name, phone}
    */
 
-  static async register({username, password, first_name, last_name, phone}) { }
+  static async register({username, password, first_name, last_name, phone}) {
+    const result = await db.query(
+      `INSERT INTO users (username, password, first_name, last_name, phone, join_at, last_login_at)
+               VALUES ($1, $2, $3, $4, $5, current_timestamp, current_timestamp)
+               RETURNING username, first_name, last_name, phone`,
+        [username, password, first_name, last_name, phone]);
+    return result.rows[0];   
+   }
 
   /** Authenticate: is this username/password valid? Returns boolean. */
 
-  static async authenticate(username, password) { }
+  static async authenticate(username, password) {
+    const result = await db.query(
+      `SELECT username
+       FROM users
+       WHERE username = $1 AND password = $2`,
+       [username, password]);
+    
+   }
 
   /** Update last_login_at for user */
 
-  static async updateLoginTimestamp(username) { }
+  static async updateLoginTimestamp(username) {
+    const result = await db.query(
+      `UPDATE users
+       SET last_login_at = current_timestamp
+       WHERE username = $1
+       RETURNING username, last_login_at`,
+       [username]); 
+   }
 
   /** All: basic info on all users:
    * [{username, first_name, last_name, phone}, ...] */
 
-  static async all() { }
+  static async all() {
+    const result = await db.query(
+      `SELECT username, first_name, last_name, phone
+       FROM users`);
+    return result.rows;
+   }
 
   /** Get: get user by username
    *
@@ -34,7 +60,14 @@ class User {
    *          join_at,
    *          last_login_at } */
 
-  static async get(username) { }
+  static async get(username) {
+    const result = await db.query(
+      `SELECT username, first_name, last_name, phone, join_at, last_login_at
+       FROM users
+       WHERE username = $1`,
+       [username]);
+    return result.rows[0];
+   }
 
   /** Return messages from this user.
    *
@@ -44,7 +77,15 @@ class User {
    *   {username, first_name, last_name, phone}
    */
 
-  static async messagesFrom(username) { }
+  static async messagesFrom(username) {
+    const result = await db.query(
+      `SELECT m.id, m.to_username, m.body, m.sent_at, m.read_at, u.username, u.first_name, u.last_name, u.phone
+       FROM messages AS m
+       JOIN users AS u ON m.to_username = u.username
+       WHERE from_username = $1`,
+       [username]);
+    return result.rows;
+   }
 
   /** Return messages to this user.
    *
@@ -54,7 +95,15 @@ class User {
    *   {username, first_name, last_name, phone}
    */
 
-  static async messagesTo(username) { }
+  static async messagesTo(username) {
+      const result = await db.query)
+        `SELECT m.id, m.from_username, m.body, m.sent_at, m.read_at, u.username, u.first_name, u.last_name, u.phone`
+        `FROM messages AS m
+        JOIN users AS u ON m.from_username = u.username
+        WHERE to_username = $1`,
+        [username]);
+      return result.rows;
+   }
 }
 
 
--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,24 @@
+const { router } = require("../app");
+
 /** POST /login - login: {username, password} => {token}
  *
  * Make sure to update their last-login!
  *
  **/
-
+router.post("/login", async function(req, res, next) {
+  try {
+    const { username, password } = req.body;
+    if (await User.authenticate(username, password)) {
+      await User.updateLoginTimestamp(username);
+      let token = jwt.sign({ username }, SECRET_KEY);
+      return res.json({ token });
+    } else {
+      throw new ExpressError("Invalid username/password", 400);
+    }
+  } catch (err) {
+    return next(err);
+  }
+});
 
 /** POST /register - register user: registers, logs in, and returns token.
  *
